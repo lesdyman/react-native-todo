d7322d1ad0d291f86b214c53f3bde789d0e7e2af
Les Dyman <lesdyman@gmail.com>
2024-09-13 15:31:13 +0200

    add initial solution

--- App.tsx
+++ App.tsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState, useRef } from 'react';
 import {
+  Image,
+  Pressable,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-
 import { styles } from './styles';
 import { SwipeListView } from 'react-native-swipe-list-view';
 
-
 function App(): React.JSX.Element {
   const initial_todos = [
-    { key: '1', text: 'Buy it', color: '#780000'},
-    { key: '2', text: 'Use it', color: '#FDF0D5'},
-    { key: '3', text: 'Trash it', color: '#003049'},
-    { key: '4', text: 'Fix it', color: '#669BBC'},
+    { key: '1', text: 'Buy it', color: '#780000', done: false },
+    { key: '2', text: 'Use it', color: '#fdc500', done: false },
+    { key: '3', text: 'Trash it', color: '#003049', done: true },
+    { key: '4', text: 'Fix it', color: '#669BBC', done: false },
   ];
 
   interface Todo {
-    key: string,
-    text: string,
-    color: string
+    key: string;
+    text: string;
+    color: string;
+    done: boolean;
   }
 
   const [todos, setTodos] = useState<Todo[]>(initial_todos);
   const [inputValue, setInputValue] = useState<string>('');
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editItemKey, setEditItemKey] = useState<string>('');
+  const [editingValue, setEditingValue] = useState<string>('');
+
+  const rowRefs = useRef<{ [key: string]: any }>({});
 
   const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
@@ -38,13 +45,63 @@ function App(): React.JSX.Element {
 
   const handleAddTodo = () => {
     if (inputValue.trim()) {
-      setTodos([...todos, { key: String(todos.length + 1), text: inputValue, color: getRandomColor() }]);
-      setInputValue('');
+      if (isEditing && editItemKey) {
+        setTodos(
+          todos.map(todo =>
+            todo.key === editItemKey
+              ? { ...todo, text: inputValue }
+              : todo
+          )
+        );
+        setIsEditing(false);
+        setEditItemKey('');
+        setInputValue('');
+      } else {
+        setTodos([
+          ...todos,
+          {
+            key: String(todos.length + 1),
+            text: inputValue,
+            color: getRandomColor(),
+            done: false,
+          },
+        ]);
+        setInputValue('');
+      }
+    }
+  };
+
+  const handleUpdate = (itemId: string) => {
+    if (editingValue.trim()) {
+      setTodos(
+        todos.map(todo =>
+          todo.key === itemId
+            ? { ...todo, text: editingValue }
+            : todo
+        )
+      );
+      setIsEditing(false);
+      setEditItemKey('');
+      setEditingValue('');
     }
   };
 
   const handleDeleteTodo = (itemId: string) => {
     setTodos(todos.filter(todo => todo.key !== itemId));
+  };
+
+  const handleEditTodo = (itemId: string) => {
+    setIsEditing(true);
+    setEditItemKey(itemId);
+    setEditingValue(todos.find(todo => todo.key === itemId)?.text || '');
+  };
+
+  const toggleStatus = (itemId: string) => {
+    setTodos(
+      todos.map(item =>
+        item.key === itemId ? { ...item, done: !item.done } : item
+      )
+    );
   };
 
   return (
@@ -59,28 +116,85 @@ function App(): React.JSX.Element {
           onChangeText={setInputValue}
         />
         <TouchableOpacity style={styles.addButton} onPress={handleAddTodo}>
-          <Text style={styles.addButtonText}>Add</Text>
+          <Text style={styles.addButtonText}>
+            Add
+          </Text>
         </TouchableOpacity>
       </View>
 
       <SwipeListView
-       disableRightSwipe={true}
+        disableRightSwipe={true}
         contentContainerStyle={styles.todoList}
-        rightOpenValue={-90}
-        stopRightSwipe={-90}
+        rightOpenValue={-180}
+        stopRightSwipe={-180}
         data={todos}
         renderItem={({ item }) => (
-          <View style={[styles.todoItem, { backgroundColor: item.color}]}>
-            <Text style={styles.todoText}>{item.text}</Text>
-          </View>
+          <Pressable onPress={() => toggleStatus(item.key)}>
+            {isEditing && editItemKey === item.key ? (
+              <View style={styles.editView}>
+                <TextInput
+                  value={editingValue}
+                  style={styles.editInput}
+                  onChangeText={setEditingValue}
+                />
+                <TouchableOpacity
+                  style={styles.addButton}
+                  onPress={() => {
+                    handleUpdate(item.key);
+                  }}
+                >
+                  <Text style={styles.addButtonText}>Update</Text>
+                </TouchableOpacity>
+              </View>
+            ) : (
+              <View
+                style={[
+                  styles.todoItem,
+                  { backgroundColor: item.done ? '#adb5bd' : item.color },
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.todoText,
+                    {
+                      textDecorationLine: item.done ? 'line-through' : 'none',
+                    },
+                  ]}
+                >
+                  {item.text}
+                </Text>
+                {item.done && (
+                  <Image
+                    source={require('./assets/check.png')}
+                    style={styles.checkIcon}
+                  />
+                )}
+              </View>
+            )}
+          </Pressable>
         )}
-        renderHiddenItem={(data) => (
-          <View>
-            <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteTodo(data.item.key)}>
-              <Text style={styles.deleteButtonText}>Delete</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        renderHiddenItem={(data, rowMap) => {
+          if (isEditing && editItemKey === data.item.key) {
+            return null;
+          }
+          rowRefs.current[data.item.key] = rowMap[data.item.key];
+          return (
+            <View style={styles.hiddenButtons}>
+              <TouchableOpacity
+                style={styles.editButton}
+                onPress={() => handleEditTodo(data.item.key)}
+              >
+                <Text style={styles.buttonText}>Edit</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.deleteButton}
+                onPress={() => handleDeleteTodo(data.item.key)}
+              >
+                <Text style={styles.buttonText}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
         keyExtractor={item => item.key}
       />
     </View>
--- styles.ts
+++ styles.ts
@@ -34,6 +34,20 @@ export const styles = StyleSheet.create({
     shadowRadius: 3,
     elevation: 1,
   },
+  editView: {
+    flexDirection: 'row',
+  },
+  editInput: {
+    fontSize: 20,
+    height: 70,
+    width: '75%',
+    borderColor: '#ddd',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    marginRight: 5,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
   addButton: {
     height: 70,
     width: 80,
@@ -46,15 +60,30 @@ export const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
+  hiddenButtons: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#ddd',
+    margin: 1,
+    marginHorizontal: 4,
+    borderRadius: 5,
+  },
   deleteButton: {
     padding: 25,
-    borderRadius: 10,
-    borderColor: '#ddd',
-    borderWidth: 1,
+    margin: 1,
     marginBottom: 2,
-    backgroundColor: '#cc0000',
+    backgroundColor: '#e74c3c',
+    borderRadius: 5,
   },
-  deleteButtonText: {
+  editButton: {
+    padding: 25,
+    margin: 1,
+    marginBottom: 2,
+    backgroundColor: '#48cae4',
+    borderRadius: 5,
+  },
+  buttonText: {
     color: '#fff',
     fontSize: 16,
     textAlign: 'right',
@@ -63,6 +92,9 @@ export const styles = StyleSheet.create({
     flexGrow: 1,
   },
   todoItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
     padding: 25,
     borderRadius: 10,
     borderColor: '#ddd',
@@ -70,8 +102,12 @@ export const styles = StyleSheet.create({
     marginBottom: 2,
   },
   todoText: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: '500',
     color: '#DFDFDF',
+  },
+  checkIcon: {
+    width: 20,
+    height: 20,
   },
 });
